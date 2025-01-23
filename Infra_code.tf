6c5d20fbbc999b84940bc6cd9d84b8534b30e72b
ramesh2024-hub <158137947+ramesh2024-hub@users.noreply.github.com>
2025-01-23 15:45:49 +1100

    Update Infra_code.tf

--- Infra_code.tf
+++ Infra_code.tf
@@ -4,14 +4,8 @@ providers "aws"{
   region = "us-west-2"
 }
 
-Add=Subnet_Branch-3
-
-/* Add Subnet */
-
-/*
-Added the Internet  Gateway - Code using the branch-1
-*/
-
+/* Added the Internet  Gateway - Code using the branch-1 */
 
 /* Add VPC - Code using the branch -2*/
 
+/* Add Subnet Code using the branch-3 */
